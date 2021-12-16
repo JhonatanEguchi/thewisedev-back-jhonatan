6c39d292979e64be43f4a6b22fdde4ef507d9c4e
Jhonatan Eguchi <hiroo.eguchi@unifesp.br>
2021-12-16 16:20:56 -0300

    feat: new features

---
+++ test/entitites/course.spec.ts
@@ -0,0 +1,13 @@
+import { Course, Module, Lecture } from '../../src/entities'
+
+describe('Course', () => {
+  it('should be able to add modules do courses', () => {
+    const course = new Course('azure-devops',
+      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+    const module = new Module('Fundamentals')
+    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    module.add(lecture)
+    course.add(module)
+    expect(course.includes(module)).toBeTruthy()
+  })
+})
