255a3c7cda54fd43f22d4b749d9c47a3e63694f9
Jhonatan Eguchi <hiroo.eguchi@unifesp.br>
2021-12-16 14:47:07 -0300

    chore: change module and lecture

--- src/entities/module.ts
+++ src/entities/module.ts
@@ -1,5 +1,13 @@
+import { Lecture } from './lecture'
+
 export class Module {
-  constructor(
-        readonly name: string
-    )
+    readonly lectures: Array<Lecture> = []
+    private name: string
+    constructor (name: string) {
+      this.name = name
+    }
+
+    add (lecture: Lecture): void {
+      this.lectures.push(lecture)
+    }
 }
