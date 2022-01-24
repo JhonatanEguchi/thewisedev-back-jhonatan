9148b1167c9da553bb52778eb8b4f0234443be10
Jhonatan Eguchi <hiroo.eguchi@unifesp.br>
2022-01-24 16:57:02 -0300

    feat: test

--- test/entitites/lecture.spec.ts
+++ test/entitites/lecture.spec.ts
@@ -1,4 +1,5 @@
 import { Lecture } from '../../src/entities'
+import { Link } from '../../src/entities/link'
 import { Material } from '../../src/entities/material'
 import { Pdf } from '../../src/entities/pdf'
 
@@ -10,11 +11,18 @@ describe('Lecture', () => {
     expect(lecture.includes(branchingPdf)).toBeTruthy()
   })
 
-  it('should be able to add further material to lectures', () => {
+  it('should be able to remove further material to lectures', () => {
     const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
     const branchingPdf: Material = new Pdf('Brnaching', 'https://storage/branching.pdf')
     lecture.add(branchingPdf)
     lecture.remove(branchingPdf)
     expect(lecture.includes(branchingPdf)).toBeFalsy()
+  })
+
+  it('should be able to add further links to lectures', () => {
+    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    const branchingLink: Material = new Link('Brnaching', 'https://storage/branching.pdf')
+    lecture.add(branchingLink)
+    expect(lecture.includes(branchingLink)).toBeTruthy()
   })
 })
